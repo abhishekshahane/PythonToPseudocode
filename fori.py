1f6825cc1b5af44f2fdc10d70765aca03945f9f5
theabhishekshahane <38487181+abhishekshahane@users.noreply.github.com>
2020-10-09 22:39:15 +0530

    Create fori.py

---
+++ fori.py
@@ -0,0 +1,14 @@
+from re import sub
+# I was gonna use lambda functions but honestly who cares
+# BTW docker.txt and out.txt are tests
+# This whole project is still in development, so don't use it yet!!
+def main(a):
+    b = a.split(" ")
+    b[0] = sub(b[0], "FOR", b[0])
+    b[3] =  b[3].replace("range(", "")
+    b[3] = b[3].replace(")", "")
+    b[3] = b[3].replace(",", " TO ")
+    b[3] = b[3].replace(":", "")
+    b[2] = sub(b[2], "<-", b[2])
+    makefinal = " ".join(b)
+    return makefinal
