b0de82f0fd1f3c804832bc33baca40652f3e3169
theabhishekshahane <38487181+abhishekshahane@users.noreply.github.com>
2020-10-10 01:56:21 +0530

    Update make.py

--- make.py
+++ make.py
@@ -2,19 +2,25 @@ from re import sub
 import fori
 import initialise
 import out
+import increment
 f = open("text/docker.txt", "r")
 file1 = open('output/out.txt', "w")
 for x in f:
     # Assuming there aren't a for loop in a if statement and so on
-    if x.find("=")!=-1:
+    # Edge case
+    if x.find("=")!=-1 and x.find("+=")==-1:
         make = initialise.main(x)
         file1.write(make)
-    elif x.find("for")!=-1:
-        makea = fori.main(x)
+    #New route for each different scenario
+    elif x.find("+=")!=-1:
+        makea = increment.main(x)
         file1.write(makea)
-    elif x.find("print")!=-1:
-        makeb = out.output(x)
+    elif x.find("for")!=-1:
+        makeb = fori.main(x)
         file1.write(makeb)
+    elif x.find("print")!=-1:
+        makec = out.output(x)
+        file1.write(makec)
 
         
 
