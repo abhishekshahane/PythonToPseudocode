aa6f5fcc44d3586cfc3fd4be17f56d37502168b9
theabhishekshahane <38487181+abhishekshahane@users.noreply.github.com>
2020-10-09 23:57:22 +0530

    Made different route for basic print

--- make.py
+++ make.py
@@ -1,6 +1,7 @@
 from re import sub
 import fori
 import initialise
+import out
 f = open("text/docker.txt", "r")
 file1 = open('output/out.txt', "w")
 for x in f:
@@ -11,3 +12,9 @@ for x in f:
     elif x.find("for")!=-1:
         makea = fori.main(x)
         file1.write(makea)
+    elif x.find("print")!=-1:
+        makeb = out.output(x)
+        file1.write(makeb)
+
+        
+
