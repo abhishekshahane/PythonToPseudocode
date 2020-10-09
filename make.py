72d047c5e503ca0f117d9b0df6318392857118e3
theabhishekshahane <38487181+abhishekshahane@users.noreply.github.com>
2020-10-09 23:24:28 +0530

    Update make.py

--- make.py
+++ make.py
@@ -3,20 +3,11 @@ import fori
 import initialise
 f = open("text/docker.txt", "r")
 file1 = open('output/out.txt', "w")
-a = f.readline()
-b = f.readline()
-c = f.readline()
-d = f.readline()
-if a.find("=")!=-1:
-    make = initialise.main(a)
-    file1.write(make)
-if b.find("for")!=-1:
-    makea = fori.main(b)
-    file1.write(makea)
-if c.find("for")!=-1:
-    makea = fori.main(c)
-    file1.write(makea)
-if d.find("=")!=-1:
-    make = initialise.main(d)
-    file1.write(make)
-
+for x in f:
+    # Assuming there aren't a for loop in a if statement and so on
+    if x.find("=")!=-1:
+        make = initialise.main(x)
+        file1.write(make)
+    elif x.find("for")!=-1:
+        makea = fori.main(x)
+        file1.write(makea)
