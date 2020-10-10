07dfc2aa26e1df66f82cc2a9156f586b6fb5b969
theabhishekshahane <38487181+abhishekshahane@users.noreply.github.com>
2020-10-10 12:14:03 +0530

    fixed some bugs in the for loop

--- fori.py
+++ fori.py
@@ -10,5 +10,7 @@ def main(a):
     b[3] = b[3].replace(",", " TO ")
     b[3] = b[3].replace(":", "")
     b[2] = sub(b[2], "<-", b[2])
-    makefinal = " ".join(b)
-    return makefinal
+    b[4] = b[4].replace(")", "")
+    b[4] = b[4].replace(":", "")
+    return f"{b[0]} {b[1]} {b[2]} {b[3]} {b[4]}"
+    
