35c8b892a2cad226dc7594591a9c23d2e7acf780
theabhishekshahane <38487181+abhishekshahane@users.noreply.github.com>
2020-10-10 12:19:46 +0530

    Create setup.py

---
+++ setup/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+import make
+
+setup(
+    name='PythonToPseudocode',
+    description='Converts Python to Pseudocode. Still in development!',
+    author='Abhishek Shahane',
+    scripts=['make.py']
+)
