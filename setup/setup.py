0372ed0ddfc393f3750edda5ea3b11d8ea1d807e
theabhishekshahane <38487181+abhishekshahane@users.noreply.github.com>
2020-10-13 22:11:27 +0530

    Update setup.py

--- setup/setup.py
+++ setup/setup.py
@@ -5,5 +5,6 @@ setup(
     name='PythonToPseudocode',
     description='Converts Python to Pseudocode. Still in development!',
     author='Abhishek Shahane',
+    year='2020'
     scripts=['make.py']
 )
