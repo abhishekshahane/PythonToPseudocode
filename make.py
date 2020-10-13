54ae030ce1caf5615b894cf4b772feddfee8335a
theabhishekshahane <38487181+abhishekshahane@users.noreply.github.com>
2020-10-13 22:09:43 +0530

    Added STEP, DO

--- make.py
+++ make.py
@@ -9,7 +9,7 @@ file1 = open('output/out.txt', "w")
 for x in f:
     # Assuming there aren't a for loop in a if statement and so on
     # Edge case
-    if x.find("=")!=-1 and x.find("+=")==-1 and x.find("-=")==-1:
+    if x.find("=")!=-1 and x.find("+=")==-1 and x.find("-=")==-1 and x.find("==")==-1 and x.find(">=")==-1 and x.find("<=")==-1 and x.find("!=")==-1:
         make = initialise.main(x)
         file1.write(make)
     elif x.find("+=")!=-1:
@@ -17,7 +17,8 @@ for x in f:
         file1.write(makea)
     elif x.find("for")!=-1:
         makeb = fori.main(x)
-        file1.write(makeb)
+        a = f"{makeb[0]} {makeb[1]}{makeb[2]} {makeb[3]} {makeb[4]} {makeb[5]}\n"
+        file1.write(a)
     elif x.find("print")!=-1:
         makec = out.output(x)
         file1.write(makec)
@@ -27,6 +28,16 @@ for x in f:
     elif x.find("-=")!=-1:
         makee = increment.decrement(x)
         file1.write(makee)
+    elif x.find("while")!=-1:
+        makef = fori.whileloop(x)
+        a = f"{makef[0]} {makef[1]}=={makef[2]} {makef[3]}\n"
+        file1.write(a)
+        
+file1.close()
+        
+
+        
+
 
         
 
