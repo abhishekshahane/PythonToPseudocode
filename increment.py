3f68b26c85b6d9d4d2f3539fbe2a3af7e25c8549
theabhishekshahane <38487181+abhishekshahane@users.noreply.github.com>
2020-10-17 18:10:56 +0530

    added new things

--- increment.py
+++ increment.py
@@ -1,5 +1,5 @@
 from re import sub, findall
-from extrafuncs import check, alphnum
+from extrafuncs import check, String
 def main(a):
     b = a.split("+=")
     return f"{b[0]}<- {b[0]} + {b[1]}"
@@ -11,17 +11,19 @@ def ifstatement(a):
     a=str(a)
     a = a.replace(":", "")
     c = findall(r"[\w']+", a)
-    f = alphnum(a)
+    b = String(a)
+    b = b.alphanum()
     c[0] = sub(c[0], "IF", c[0])
     j = list(map(lambda s: s.strip(), c))
-    f = f.strip()
+    f=b.strip()
     return "{} {} {} {}".format(j[0], j[1], f, j[2])
 def elseif(a):
     a=str(a)
     a = a.replace(":", "")
     c = findall(r"[\w']+", a)
-    f = alphnum(a)
+    b = String(a)
+    b = b.alphanum()
     c[0] = sub(c[0], "ELSE IF", c[0])
     j = list(map(lambda s: s.strip(), c))
-    f = f.strip()
+    f=b.strip()
     return "{} {} {} {}".format(j[0], j[1], f, j[2])
