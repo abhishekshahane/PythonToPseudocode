60b425d9a6bcf3051a05eb049db0cf711592893d
theabhishekshahane <38487181+abhishekshahane@users.noreply.github.com>
2020-10-17 18:10:17 +0530

    added a class

--- extrafuncs.py
+++ extrafuncs.py
@@ -1,14 +1,14 @@
 def check(f):
-    while('' in f) : 
+    while('' in f) :
         f.remove('')
-    return f 
-def alphnum(f):
-    stra=""
-    c = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_'
-    for i in range(len(f)):
-        if f[i] not in c and f[i] not in ' ':
-            stra+=f[i]
-    return stra
-
-
-
+    return f
+class String:
+    def __init__(self, give):
+        self.give = give
+    def alphanum(self):
+        stra=""
+        c = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_'
+        for i in range(len(self.give)):
+            if self.give[i] not in c and self.give[i] not in ' ':
+                stra+=self.give[i]
+        return stra
