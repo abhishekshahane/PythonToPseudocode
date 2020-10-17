f3b06b13edb9291bb9bd5c98f731bb09eb5dca13
theabhishekshahane <38487181+abhishekshahane@users.noreply.github.com>
2020-10-17 18:08:11 +0530

    Added classes

--- extrafuncs.py
+++ extrafuncs.py
@@ -2,13 +2,6 @@ def check(f):
     while('' in f) :
         f.remove('')
     return f
-def alphnum(f):
-    stra=""
-    c = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_'
-    for i in range(len(f)):
-        if f[i] not in c and f[i] not in ' ':
-            stra+=f[i]
-    return stra
 class String:
     def __init__(self, give):
         self.give = give
--- increment.py
+++ increment.py
@@ -1,5 +1,5 @@
 from re import sub, findall
-from extrafuncs import check, alphnum, String
+from extrafuncs import check, String
 def main(a):
     b = a.split("+=")
     return f"{b[0]}<- {b[0]} + {b[1]}"
@@ -21,8 +21,9 @@ def elseif(a):
     a=str(a)
     a = a.replace(":", "")
     c = findall(r"[\w']+", a)
-    f = alphnum(a)
+    b = String(a)
+    b = b.alphanum()
     c[0] = sub(c[0], "ELSE IF", c[0])
     j = list(map(lambda s: s.strip(), c))
-    f = f.strip()
+    f=b.strip()
     return "{} {} {} {}".format(j[0], j[1], f, j[2])
